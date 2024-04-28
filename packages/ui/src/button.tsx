f49df56663b400c3a483511791ca7b1c4c474e17
Matthieu Compérat <matthieu.comperat@gmail.com>
2024-04-28 18:21:33 +0200

    tech(storybook): init storybook

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,12 +5,21 @@ import { ReactNode } from 'react';
 interface ButtonProps {
   children: ReactNode;
   className?: string;
+  primary?: boolean;
+  size?: 'small' | 'large';
   appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, primary, size, appName }: ButtonProps) => {
   return (
-    <button className={className} onClick={() => alert(`Hello from your ${appName} app!`)}>
+    <button
+      className={className}
+      style={{
+        backgroundColor: primary ? 'red' : 'blue',
+        fontSize: size === 'large' ? '24px' : '14px',
+      }}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
+    >
       {children}
     </button>
   );
